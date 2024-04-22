4d4fa6fdf149424b71941a3d43078eaf919c4ab5
Lan Liang <gcslyp@gmail.com>
2024-04-22 09:19:13 +0000

    u

--- registry.k8s.io/images/findvolume/main.go
+++ registry.k8s.io/images/findvolume/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 // {"Size":5003804672,"Head":"volume-head-000.img","Dirty":true,"Rebuilding":false,"Error":"","Parent":"","SectorSize":512,"BackingFilePath":""}
@@ -23,17 +24,23 @@ type VolumeMeta struct {
 func main() {
 	rootPath := os.Getenv("FIND_PATH")
 	printAll := os.Getenv("PRINT_ALL")
+	skipVolumesStr := os.Getenv("SKIP_VOLUMES")
+	skipVolumes := []string{}
+	if skipVolumesStr != "" {
+		skipVolumes = strings.Split(skipVolumesStr, ",")
+	}
 	if rootPath == "" {
 		log.Fatalln("empty rootPath!!,set it into the env FIND_PATH")
 	}
 	if printAll == "" {
-		printAll = "1"
+		printAll = "0"
 	}
 	// rootPath := "/home/runner/work/lanactions/lanactions/k8s.io/registry.k8s.io/images/tests/"
 	// TODO 拼凑docker命令
 	//  docker run -v /dev:/host/dev -v /proc:/host/proc -v /var/lib/longhorn/replicas/pvc-c71e014b-fe06-463a-82f7-c9da74c55cdb-9339ab17:/volume --privileged longhornio/longhorn-engine:v1.6.1 launch-simple-longhorn pvc-c71e014b-fe06-463a-82f7-c9da74c55cdb 10737418240
 	fss, err := os.ReadDir(rootPath)
 	checkErr(err)
+	t2 := time.Date(2024, time.January, 1, 1, 0, 0, 0, time.UTC)
 	for _, f := range fss {
 		if f.IsDir() {
 			// log.Println("fs.path:", f.Name())
@@ -46,10 +53,30 @@ func main() {
 				log.Printf("dirty:%t,rebuilding:%t \n", v.Dirty, v.Rebuilding)
 			} else {
 				if !v.Dirty && !v.Rebuilding {
-					log.Println("fs.path is not dirty and rebuilding:", f.Name())
+
 					splitStrs := strings.Split(f.Name(), "-")
 					replicasName := splitStrs[len(splitStrs)-1]
 					pvcName := strings.ReplaceAll(f.Name(), "-"+replicasName, "")
+					if len(skipVolumes) > 0 {
+						skip := false
+						for _, sv := range skipVolumes {
+							if sv == pvcName {
+								skip = true
+								break
+							}
+						}
+						if skip {
+							continue
+						}
+					}
+					fsi, err := f.Info()
+					if err != nil {
+						log.Println("get modTime failed!", f.Name(), err)
+						continue
+					}
+					// log.Println(fsi.ModTime().After(t2))
+
+					log.Println("fs.path is not dirty and rebuilding:", f.Name(), fsi.ModTime(), fsi.ModTime().After(t2))
 					dockerCommand := fmt.Sprintf("docker run -v /dev:/host/dev -v /proc:/host/proc -v %s%s:/volume --privileged longhornio/longhorn-engine:v1.6.1 launch-simple-longhorn %s %d", rootPath, f.Name(), pvcName, v.Size)
 					log.Println("got the command:\n", dockerCommand)
 				}
